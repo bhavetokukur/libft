a377271feb4b307e41eb75e87abbfd19b8a6bcf2
Timur Haziiev <thaziiev@c1r6s6.42london.com>
2024-05-17 22:45:37 +0100

    mandatory all green

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,17 @@
 /*   By: thaziiev <thaziiev@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 14:05:24 by thaziiev          #+#    #+#             */
-/*   Updated: 2024/05/17 19:44:33 by thaziiev         ###   ########.fr       */
+/*   Updated: 2024/05/17 22:31:38 by thaziiev         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int words_num(char const *s, char c)
+int	words_num(char const *s, char c)
 {
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	char 	**arr;
-	
 
 	i = 0;
 	j = 0;
@@ -31,11 +29,10 @@ int words_num(char const *s, char c)
 			i++;
 		if (i > k)
 		{
-			arr[j] = k, i - k, j;
 			j++;
 		}
 	}
-	return (arr);
+	return (j);
 }
 
 char	**ft_split(char const *s, char c)
@@ -44,12 +41,10 @@ char	**ft_split(char const *s, char c)
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	int 	words;
 
 	i = 0;
 	j = 0;
-	words = words_num(s, c);
-	arr = (char **)malloc(sizeof(char *) * (words + 1));
+	arr = (char **)malloc(sizeof(char *) * (words_num(s, c) + 1));
 	if (!arr || !s)
 		return (NULL);
 	while (s[i])
