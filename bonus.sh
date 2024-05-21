a1b84f40e8ffea886a10ab31cd84fe55c1f6f874
Timur Haziiev <thaziiev@c1r3s6.42london.com>
2024-05-21 17:23:59 +0100

    deleted unnecessary

--- bonus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -f a.out
-make clean
-make bonus
-echo "---------------------$1------------------------"
-cc -Wall -Wextra -Werror -o a.out ./mytest/ft_$1_test.c -L. -lft
-echo "---------------------Om------------------------"
-./a.out
--- mytest/ft_strdup_test.c
+++
@@ -1,9 +0,0 @@
-#include "../libft.h"
-
-int main(void)
-{
-    char *s = "hi";
-    char *p = ft_strdup(s);
-    printf("%s\n", p);
-    return 0;
-}
